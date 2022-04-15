83b2cf8f39e67ce2286499a0f9c24b77c667c8db
Pallas <pallassgj@163.com>
2022-04-16 00:33:52 +0800

    cross request

--- src/main/java/top/minicreo/cloudmusic/controller/HelloController.java
+++ src/main/java/top/minicreo/cloudmusic/controller/HelloController.java
@@ -1,9 +1,10 @@
 package top.minicreo.cloudmusic.controller;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HelloController {
 	@GetMapping("/sayHello")
 	public String sayHello() {
